6891c113ee580dd0031087d3474697df71374050
Marcos Lautaro Molina <marcoslautaromolina@gmail.com>
2025-12-10 02:17:27 -0300

    implementamos la funcion de buscar y editar un servicio en el componente FormularioServicio

--- src/components/pages/FormularioServicio.jsx
+++ src/components/pages/FormularioServicio.jsx
@@ -1,15 +1,37 @@
+import { useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useForm } from "react-hook-form";
+import { useParams } from "react-router";
 import Swal from "sweetalert2";
 
-const FormularioServicio = ({ titulo, crearServicio, editarServicio }) => {
+const FormularioServicio = ({
+  titulo,
+  crearServicio,
+  editarServicio,
+  buscarServicio,
+}) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-    reset
+    reset,
+    setValue,
   } = useForm();
+  const { id } = useParams(); // obtengo el id de la url
+
+  useEffect(() => {
+    // si estoy editando entonces busco el objeto para dibujar en el formulario
+    if (titulo === "Editar servicio") {
+      const servicioBuscado = buscarServicio(id);
+      setValue("servicio", servicioBuscado.servicio);
+      setValue("precio", servicioBuscado.precio);
+      setValue("imagen", servicioBuscado.imagen);
+      setValue("categoria", servicioBuscado.categoria);
+      setValue("descripcion_breve", servicioBuscado.descripcion_breve);
+      setValue("descripcion_amplia", servicioBuscado.descripcion_amplia);
+    }
+  }, []);
 
   const onSubmit = (data) => {
     console.log(data);
@@ -21,9 +43,15 @@ const FormularioServicio = ({ titulo, crearServicio, editarServicio }) => {
         text: `El servicio '${data.servicio}' fue creado correctamente`,
         icon: "success",
       });
-      reset()
+      reset();
     } else {
       //agregar la logica para editar
+      editarServicio(id, data);
+      Swal.fire({
+        title: "Servicio editado",
+        text: `El servicio '${data.servicio}' fue editado correctamente`,
+        icon: "success",
+      });
     }
   };
 
